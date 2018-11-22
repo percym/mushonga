8ee8adc871de3140cdbb713e0025c3ed5da46446
percy mugadza <percym@localhost.localdomain>
2018-11-22 09:37:11 +0200

    loading endpoint

---
+++ dashboard-web/src/main/java/systems/health263/dashboard/DashboardWebApplication.java
@@ -0,0 +1,12 @@
+package systems.health263.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardWebApplication.class, args);
+	}
+}
---
+++ dashboard-web/src/test/java/systems/health263/dashboard/DashboardWebApplicationTests.java
@@ -0,0 +1,16 @@
+package systems.health263.dashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DashboardWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
