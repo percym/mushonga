42d3f328d7c02ba41a5f35a0f46cafa079db31ca
percy mugadza <percym@localhost.localdomain>
2018-11-22 16:49:56 +0200

    added locations

---
+++ dashboard-repository/src/main/java/systems/health263/dashboard/repository/location/LocationRepository.java
@@ -0,0 +1,17 @@
+package systems.health263.dashboard.repository.location;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+import systems.health263.dashboard.model.location.Location;
+
+/**
+ * @author Munyaradzi Takayindisa
+ * <p>
+ * Repository for Group entity
+ */
+@Repository
+public interface LocationRepository extends JpaRepository<Location, Long>, JpaSpecificationExecutor<Location> {
+    
+}
