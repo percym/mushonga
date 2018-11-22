ac87a44d15bce30ca616a7c71513267a1f61e41c
percy mugadza <percym@localhost.localdomain>
2018-11-22 13:16:00 +0200

    working added swagger

--- dashboard-web/src/main/java/systems/health263/dashboard/DashboardWebApplication.java
+++ dashboard-web/src/main/java/systems/health263/dashboard/DashboardWebApplication.java
@@ -1,20 +1,33 @@
 package systems.health263.dashboard;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@ComponentScan({"systems.health263.dashboard"})
+@EnableAutoConfiguration(exclude={
+        DataSourceTransactionManagerAutoConfiguration.class})
+@ComponentScan(basePackages = {"systems.health263.dashboard","systems.health263.dashboard.endpoint"})
 @EntityScan({"systems.health263.dashboard"})
 @EnableJpaRepositories({"systems.health263.dashboard"})
 @SpringBootApplication
-public class DashboardWebApplication {
+public class DashboardWebApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(DashboardWebApplication.class, args);
 	}
 }
